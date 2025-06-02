4182f5f478a458967436fe9c33e35176552f9156
Zarina Ahmadjanova <Zarina05092011@gmail.com>
2025-06-02 10:39:57 +0500

    third commit

---
+++ static/js/main.js
@@ -0,0 +1,25 @@
+function scrolltoleft(button){
+    console.log(button)
+    let wrapper = document.querySelector(`#${button.getAttribute('target')} .wrapper`)
+    let w = wrapper.querySelector('.c-item').getBoundingClientRect().width
+    let currentScroll = wrapper.scrollLeft 
+
+
+    wrapper.scroll({ 
+    left: currentScroll - w,
+    behavior: "smooth", 
+    });
+}
+
+function scrolltoRight(button){
+console.log(button)
+let wrapper = document.querySelector(`#${button.getAttribute('target')} .wrapper`)
+let w = wrapper.querySelector('.c-item').getBoundingClientRect().width
+let currentScroll = wrapper.scrollLeft 
+
+
+wrapper.scroll({ 
+left: currentScroll + w,
+behavior: "smooth", 
+});
+}
