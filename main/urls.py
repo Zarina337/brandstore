d1d24fe0943b7388b2a604c4b9898a72bca787c3
Zarina Ahmadjanova <Zarina05092011@gmail.com>
2025-06-18 10:36:23 +0500

    7th

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path 
-from .views import  manepage_view ,cotegory_view
+from .views import  manepage_view ,cotegory_view, detail_view, cart_view
 app_name = 'main'
 urlpatterns = [
     path( '' ,  manepage_view , name='home'),
-    path( 'cotegory/' ,  cotegory_view , name='cotegory')
+    path( 'cotegory/' ,  cotegory_view , name='cotegory'),
+    path('detail/', detail_view, name='detail'),
+    path('cart/', cart_view, name='cart')
 ]
--- main/views.py
+++ main/views.py
@@ -8,3 +8,10 @@ def manepage_view(request):
 def cotegory_view(request):
 
     return render(request , 'cotegory.html')
+
+def detail_view(request):
+    return render(request, 'detail.html')
+
+
+def cart_view(request):
+    return render(request, 'mycart.html')
